1e7b3240fb097968c3d8a96480a86212cf4da85a
Nikita <nikita.bykov485@yandex.com>
2022-06-28 14:47:23 +0500

    Сравнение двух чисел

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Сравнение двух чисел
+Console.WriteLine("Введите два числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+Console.WriteLine($"{num1} - большее число");
+else
+Console.WriteLine($"{num2} - большее число");
